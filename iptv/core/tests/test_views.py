cdd4954509fcb1a22a22e186e9cbf305e82087d6
jonathan08 <jonathan@cosmunsoftwares.com.br>
2019-05-23 22:43:58 -0300

    first_commit

---
+++ iptv/core/tests/test_views.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse_lazy
+
+
+class HomeTest(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse_lazy('core:home'))
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'core/home.html')
